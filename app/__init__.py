b0da23a965045efd2a5314a7327d08dccaa0aeab
ahmadfathan <ahmadfathan1021@gmail.com>
2022-08-15 11:55:54 +0700

    add authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+import sys
 from http import HTTPStatus
 from os import path
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import paho.mqtt.publish as publish
+
+sys.path.insert(0, path.dirname(path.dirname(__file__)))
+
+import auth
 
 app = Flask(__name__)
 CORS(app)
@@ -20,6 +25,8 @@ MQTT_USER = config['MQTT_USER'].get()
 MQTT_PASSWORD = config['MQTT_PASSWORD'].get()
 
 RTTTL = config['RTTTL'].get()
+
+WHITELISTED_USERS = config['WHITELISTED_USERS'].get()
 
 def create_response(code, is_success, message, data=None):
     resp = {
@@ -42,6 +49,17 @@ def mqtt_publish(topic, payload):
 @app.route("/", methods=['GET'])
 def root():
     return create_response(HTTPStatus.OK, True, "It's working!!")
+
+@app.route("/auth/login", methods=['POST'])
+def login():
+    user = request.form['user']
+
+    if not user:
+        return create_response(HTTPStatus.BAD_REQUEST, True, "Bad Parameter")
+
+    token = auth.encode_auth_token(user)
+
+    return create_response(HTTPStatus.OK, True, "OK", data={token: token})
 
 @app.route("/play", methods=['POST'])
 def play():
---
+++ auth.py
@@ -0,0 +1,48 @@
+import datetime
+import jwt
+from os import path
+import confuse
+
+config = confuse.Configuration('App', __name__, read=False)
+config_path = path.join(path.dirname(__file__), 'config.yaml')
+
+config.set_file(config_path, base_for_paths=True)
+
+SECRET_KEY = config['SECRET_KEY'].get()
+
+def encode_auth_token(user_id):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+
+        return jwt.encode(
+            payload,
+            SECRET_KEY,
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+
+def decode_auth_token(auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+
+        payload = jwt.decode(auth_token, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
